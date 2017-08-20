b4a7e0fff5e11b3bf871d359a2a7a8caa28aba4e
Gopal Gupta <gopalnov19@gmail.com>
2017-08-20 13:16:15 +0530

    remove API KEYS

--- ContactMap/AppDelegate.m
+++ ContactMap/AppDelegate.m
@@ -17,8 +17,8 @@
 @synthesize managedObjectModel = _managedObjectModel;
 @synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-      [GMSServices provideAPIKey:@"AIzaSyBJEt_aRsWzOOJm7gbidB8_MgYPjVkyW2g"];
-      [GMSPlacesClient provideAPIKey:@"AIzaSyBIPcTQSlGLWeb2qfsG9MdpELS9hn4SDiY"];
+      [GMSServices provideAPIKey:@"#####ADD YOUR GOOGLE MAP KEY#######"];
+      [GMSPlacesClient provideAPIKey:@"#####ADD YOUR GOOGLE PLACES KEY#######"];
       return YES;
 }
 
